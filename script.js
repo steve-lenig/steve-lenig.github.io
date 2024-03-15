1517f56283035e0a0e5bc120bde9b8b2f15c2735
Steven Lenig <stevelenig@gmail.com>
2024-03-15 17:43:28 -0500

    no message

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("gallery-icon-link").style.display = "none";
+    document.getElementById("journal-icon-link").style.display = "none";
     document.getElementById("vhiqosad-icon-link").style.display = "none";
 
     const visitedJournal = localStorage.getItem("visitedJournal");
+    const visitedGallery = localStorage.getItem("visitedGallery");
     const visitedShop = localStorage.getItem("visitedShop");
-    if (visitedJournal || visitedShop) {
-        document.getElementById("gallery-icon-link").style.display = "block";
+    if (visitedGallery || visitedShop) {
+        document.getElementById("journal-icon-link").style.display = "block";
     }
 
     const visitedAbout = localStorage.getItem("visitedAbout");
-    if (visitedAbout) {
+    if (visitedJournal || visitedAbout) {
         document.getElementById("vhiqosad-icon-link").style.display = "block";
     }
 
     document.getElementById("journal-icon-link").addEventListener("click", function() {
-        localStorage.setItem("visitedJournal", true);
+      localStorage.setItem("visitedJournal", true);
+    });
+
+    document.getElementById("gallery-icon-link").addEventListener("click", function() {
+      localStorage.setItem("visitedGallery", true);
     });
 
     document.getElementById("shop-icon-link").addEventListener("click", function() {
-        localStorage.setItem("visitedShop", true);
+      localStorage.setItem("visitedShop", true);
     });
 
     document.getElementById("about-icon-link").addEventListener("click", function() {
-        localStorage.setItem("visitedAbout", true);
+      localStorage.setItem("visitedAbout", true);
+    });
+
+    document.getElementById("vhiqosad-icon-link").addEventListener("click", function() {
+      localStorage.setItem("visitedVhiqosad", true);
     });
 });
