6c130d1599a2a05fa67260faee4abf57f319b51e
Steven Lenig <stevelenig@gmail.com>
2024-07-21 00:28:43 -0500

    optimize

--- galleryDetail.js
+++ galleryDetail.js
@@ -32,213 +32,190 @@ const galleryData = imageMap[galleryKey];
 const title = document.getElementById('title');
 title.textContent = galleryKey;
 
-  if (!galleryData) {
-    document.body.innerHTML = '<p>No images found for the specified gallery.</p>';
-  } else {
-    const defaultTitle = galleryData.items[0].title;
-    const defaultDescription = galleryData.items[0].description;
+if (!galleryData) {
+  document.body.innerHTML = '<p>No images found for the specified gallery.</p>';
+} else {
+  const defaultTitle = galleryData.items[0].title;
+  const defaultDescription = galleryData.items[0].description;
 
-    const previewList = document.getElementById('preview-list');
-    const mainSliderContainer = document.querySelector('.slider-main');
-    const imageTitle = document.getElementById('image-title');
-    const imageDescription = document.getElementById('image-description');
+  const previewList = document.getElementById('preview-list');
+  const mainSliderContainer = document.querySelector('.slider-main');
+  const imageTitle = document.getElementById('image-title');
+  const imageDescription = document.getElementById('image-description');
 
-    // Create preview list and main viewer from data
-    galleryData.items.forEach((item) => {
-      const title = item.title || defaultTitle;
+  // Create preview list and main viewer from data
+  galleryData.items.forEach((item) => {
+    const title = item.title || defaultTitle;
 
-      const mainSlide = document.createElement('div');
-      mainSlide.classList.add('gallery__slide');
-      const mainImage = document.createElement('img');
-      mainImage.src = item.image;
-      mainImage.alt = title;
-      mainSlide.appendChild(mainImage);
-      mainSliderContainer.appendChild(mainSlide);
+    const mainSlide = document.createElement('div');
+    mainSlide.classList.add('gallery__slide');
+    const mainImage = document.createElement('img');
+    mainImage.src = item.image;
+    mainImage.alt = title;
+    mainSlide.appendChild(mainImage);
+    mainSliderContainer.appendChild(mainSlide);
 
-      const previewSlide = document.createElement('div');
-      const previewSlideContent = document.createElement('div');
-      previewSlideContent.classList.add('preview-img-content');
-      const previewImage = document.createElement('img');
-      previewImage.src = item.image;
-      previewImage.alt = title;
-      previewSlideContent.appendChild(previewImage);
-      previewSlide.appendChild(previewSlideContent);
-      previewList.appendChild(previewSlide);
+    const previewSlide = document.createElement('div');
+    const previewSlideContent = document.createElement('div');
+    previewSlideContent.classList.add('preview-img-content');
+    const previewImage = document.createElement('img');
+    previewImage.src = item.image;
+    previewImage.alt = title;
+    previewSlideContent.appendChild(previewImage);
+    previewSlide.appendChild(previewSlideContent);
+    previewList.appendChild(previewSlide);
 
-      if (galleryData.items.length <= 1) {
-        previewList.classList.add('hidden');
-      }
-    });
+    if (galleryData.items.length <= 1) {
+      previewList.classList.add('hidden');
+    }
+  });
 
-    // Create sliders
-    const mainSlider = tns({
-      container: '.slider-main',
-      items: 1,
+  // Create sliders
+  const mainSlider = tns({
+    container: '.slider-main',
+    items: 1,
+    slideBy: 'page',
+    mouseDrag: true,
+    swipeAngle: false,
+    autoplay: false,
+    nav: false,
+    controls: false,
+    mode: 'gallery'
+  });
+
+  let currentAxis = 'vertical';
+
+  function checkControlsVisibility() {
+    const isMobile = window.innerWidth <= 640;
+
+    const controlUp = document.querySelector('.control-up');
+    const controlDown = document.querySelector('.control-down');
+    const controlLeft = document.querySelector('.control-left');
+    const controlRight = document.querySelector('.control-right');
+
+    const info = previewSlider.getInfo();
+    const atStart = info.index === 0;
+    const atEnd = info.index + info.items >= info.slideCount;
+
+    controlUp.style.display = atStart ? 'none' : 'flex';
+    controlDown.style.display = atEnd ? 'none' : 'flex';
+    controlLeft.style.display = atStart ? 'none' : 'flex';
+    controlRight.style.display = atEnd ? 'none' : 'flex';
+
+    if (isMobile) {
+      controlUp.style.display = 'none';
+      controlDown.style.display = 'none';
+    } else {
+      controlLeft.style.display = 'none';
+      controlRight.style.display = 'none';
+    }
+  }
+
+  function createPreviewSlider(axis) {
+    const controlUp = document.querySelector('.control-up');
+    const controlDown = document.querySelector('.control-down');
+    const controlLeft = document.querySelector('.control-left');
+    const controlRight = document.querySelector('.control-right');
+
+    const slider = tns({
+      container: '.preview-list',
+      items: axis === 'vertical' ? 5 : 4,
       slideBy: 'page',
       mouseDrag: true,
       swipeAngle: false,
-      autoplay: false,
+      axis: axis,
+      loop: false,
+      gutter: 6,
       nav: false,
-      controls: false,
-      mode: 'gallery'
+      prevButton: axis === 'vertical' ? controlUp : controlLeft,
+      nextButton: axis === 'vertical' ? controlDown : controlRight,
     });
 
-    let currentAxis = 'vertical';
+    slider.events.on('indexChanged', checkControlsVisibility);
+    slider.events.on('scroll', checkControlsVisibility);
 
-    function checkControlsVisibility() {
-      var isMobile = window.innerWidth <= 640;
+    return slider;
+  }
 
-      const controlUp = document.querySelector('.control-up');
-      const controlDown = document.querySelector('.control-down');
-      const controlLeft = document.querySelector('.control-left');
-      const controlRight = document.querySelector('.control-right');
+  let previewSlider = createPreviewSlider(currentAxis);
 
-      const info = previewSlider.getInfo();
-      if (info.index === 0) {
-          controlUp.style.display = 'none';
-          controlLeft.style.display = 'none';
-      } else {
-          controlUp.style.display = 'flex';
-          controlLeft.style.display = 'flex';
-      }
+  const mainSliderSlides = mainSlider.getInfo().slideItems;
+  function updateSelectedPreview(index) {
+    imageTitle.textContent = galleryData.items[index].title || defaultTitle;
+    imageDescription.textContent = galleryData.items[index].description || defaultDescription;
 
-      if (info.index + info.items >= info.slideCount) {
-          controlDown.style.display = 'none';
-          controlRight.style.display = 'none';
-      } else {
-          controlDown.style.display = 'flex';
-          controlRight.style.display = 'flex';
-      }
-
-      if (isMobile) {
-        controlUp.style.display = 'none';
-        controlDown.style.display = 'none';
-      } else {
-        controlLeft.style.display = 'none';
-        controlRight.style.display = 'none';
-      }
-    }
-
-    function createPreviewSlider(axis) {
-      const controlUp = document.querySelector('.control-up');
-      const controlDown = document.querySelector('.control-down');
-      const controlLeft = document.querySelector('.control-left');
-      const controlRight = document.querySelector('.control-right');
-  
-      const slider = tns({
-        container: '.preview-list',
-        items: axis === 'vertical' ? 5 : 4,
-        slideBy: 'page',
-        mouseDrag: true,
-        swipeAngle: false,
-        // speed: 400,
-        axis: axis,
-        loop: false,
-        gutter: 6,
-        // controlsText: ['<', '>'],
-        // controls: true,
-        // nextButton: '.preview-list-next',
-        // prevButton: '.preview-list-prev',
-        nav: false,
-        // controlsContainer: '.preview-carousel-container',
-        prevButton: axis === 'vertical' ? controlUp : controlLeft,
-        nextButton: axis === 'vertical' ? controlDown : controlRight,
-      });
-
-      slider.events.on('indexChanged', checkControlsVisibility);
-      slider.events.on('scroll', checkControlsVisibility);
-
-      return slider;
-    }
-
-    let previewSlider = createPreviewSlider(currentAxis);
-
-    const mainSliderSlides = mainSlider.getInfo().slideItems;
-    function updateSelectedPreview(index) {
-      imageTitle.textContent = galleryData.items[index].title || defaultTitle
-      imageDescription.textContent = galleryData.items[index].description || defaultDescription;
-
-      const previewSliderSlides = Array.from(previewSlider.getInfo().slideItems);
-      previewSliderSlides.forEach(slide => {
-        slide.firstChild.classList.remove('preview-img-content-active');
-      });
-
-      previewSliderSlides[index].firstChild.classList.add('preview-img-content-active');
-    };
-
-    function updateMainSlider(index) {
-      mainSlider.goTo(index);
-      updateSelectedPreview(index);
-      checkControlsVisibility();
-    };
-
-    let previewSlides = Array.from(previewSlider.getInfo().slideItems);
-    previewSlides.forEach((slide, index) => {
-        slide.addEventListener('click', () => updateMainSlider(index));
-    });
-    previewSlides[0].firstChild.classList.add('preview-img-content-active');
-
-    // Set the initial title and description
-    imageTitle.textContent = defaultTitle;
-    imageDescription.textContent = defaultDescription;
-
-    mainSlider.events.on('indexChanged', (info) => {
-        const index = info.index;
-        previewSlider.goTo(index);
-        updateSelectedPreview(index);
-        addZoomEffect(mainSliderSlides[index].querySelector('img'));
-        checkControlsVisibility();
+    const previewSliderSlides = Array.from(previewSlider.getInfo().slideItems);
+    previewSliderSlides.forEach(slide => {
+      slide.firstChild.classList.remove('preview-img-content-active');
     });
 
-    function updateSliderAxis() {
-      const newAxis = window.innerWidth <= 640 ? 'horizontal' : 'vertical';
+    previewSliderSlides[index].firstChild.classList.add('preview-img-content-active');
+  }
 
-      if (currentAxis !== newAxis) {
-        currentAxis = newAxis;
-        previewSlider.destroy();
-        previewSlider = createPreviewSlider(currentAxis);
-
-        const newPreviewSliderInfo = previewSlider.getInfo();
-        const newPreviewSliderSlides = Array.from(newPreviewSliderInfo.slideItems);
-        newPreviewSliderSlides.forEach((slide, index) => {
-            slide.addEventListener('click', () => updateMainSlider(index));
-        });
-
-        const currentIndex = mainSlider.getInfo().index;
-        previewSlider.goTo(currentIndex);
-        updateSelectedPreview(currentIndex);
-        checkControlsVisibility();
-      }
-    }
-
-    window.addEventListener('resize', updateSliderAxis);
-    updateSliderAxis(); // Initial call
-
-    // Add zoom effect
-    const mainViewer = document.querySelector('.viewer');
-    const mainImage = mainViewer.querySelector('img');
-
-    function addZoomEffect(image) {
-      const mainViewer = document.querySelector('.viewer');
-
-      image.style.transition = 'transform 0.15s ease-out'; // Add transition to the new image
-
-      mainViewer.addEventListener('mousemove', (e) => {
-          const rect = mainViewer.getBoundingClientRect();
-          const x = e.clientX - rect.left; // x position within the element
-          const y = e.clientY - rect.top;  // y position within the element
-
-          // Calculate transform origin based on mouse position
-          const xPercent = (x / rect.width) * 100;
-          const yPercent = (y / rect.height) * 100;
-
-          image.style.transformOrigin = `${xPercent}% ${yPercent}%`;
-      });
-    }
-
-    // previewSlider.addEventListener('dragEnd', checkControlsVisibility());
-
-    // Apply zoom effect to the initial main image
-    addZoomEffect(mainSliderSlides[0].querySelector('img'));
+  function updateMainSlider(index) {
+    mainSlider.goTo(index);
+    updateSelectedPreview(index);
     checkControlsVisibility();
+  }
+
+  let previewSlides = Array.from(previewSlider.getInfo().slideItems);
+  previewSlides.forEach((slide, index) => {
+    slide.addEventListener('click', () => updateMainSlider(index));
+  });
+  previewSlides[0].firstChild.classList.add('preview-img-content-active');
+
+  // Set the initial title and description
+  imageTitle.textContent = defaultTitle;
+  imageDescription.textContent = defaultDescription;
+
+  mainSlider.events.on('indexChanged', (info) => {
+    const index = info.index;
+    previewSlider.goTo(index);
+    updateSelectedPreview(index);
+    addZoomEffect(mainSliderSlides[index].querySelector('img'));
+    checkControlsVisibility();
+  });
+
+  function updateSliderAxis() {
+    const newAxis = window.innerWidth <= 640 ? 'horizontal' : 'vertical';
+
+    if (currentAxis !== newAxis) {
+      currentAxis = newAxis;
+      previewSlider.destroy();
+      previewSlider = createPreviewSlider(currentAxis);
+
+      const newPreviewSliderSlides = Array.from(previewSlider.getInfo().slideItems);
+      newPreviewSliderSlides.forEach((slide, index) => {
+        slide.addEventListener('click', () => updateMainSlider(index));
+      });
+
+      const currentIndex = mainSlider.getInfo().index;
+      previewSlider.goTo(currentIndex);
+      updateSelectedPreview(currentIndex);
+      checkControlsVisibility();
+    }
+  }
+
+  window.addEventListener('resize', updateSliderAxis);
+  updateSliderAxis();
+
+  function addZoomEffect(image) {
+    const mainViewer = document.querySelector('.viewer');
+
+    image.style.transition = 'transform 0.15s ease-out';
+
+    mainViewer.addEventListener('mousemove', (e) => {
+      const rect = mainViewer.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      const xPercent = (x / rect.width) * 100;
+      const yPercent = (y / rect.height) * 100;
+
+      image.style.transformOrigin = `${xPercent}% ${yPercent}%`;
+    });
+  }
+
+  addZoomEffect(mainSliderSlides[0].querySelector('img'));
+  checkControlsVisibility();
 }
